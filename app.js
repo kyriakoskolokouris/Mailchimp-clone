7a0925634fc16251fb452434f48a09e130169fdb
kyriakos.kolokouris <kyriakoskolokouris@gmail.com>
2021-05-03 18:02:11 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// 1. Hide/show password
+
+
+const show = document.querySelector(".show");
+const hide = document.querySelector(".hide");
+const password = document.querySelector(".password");
+
+
+show.addEventListener("click", () => {
+  password.setAttribute("type", "text");
+  show.style.display = "none";
+  hide.style.display = "inline";
+  document.getElementById("password-text").textContent = "Hide";
+  hide.style.visibility = "visible";
+  password.style.border = "2px solid #007c89";
+})
+
+hide.addEventListener("click", () => {
+  password.setAttribute("type", "password");
+  hide.style.display = "none";
+  show.style.display = "inline";
+  document.getElementById("password-text").textContent = "Show";
+  password.style.border = "2px solid #007c89";
+})
